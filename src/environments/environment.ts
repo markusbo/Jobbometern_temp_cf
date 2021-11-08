9d6537c40b0b1e233227b64f4f02e8ba8ac93714
Simon Benjaminsson <benjaminsson@gmail.com>
2021-11-08 14:41:18 +0100

    move directory

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    employersApi: "https://bitter-wave-4f8f.simonbe.workers.dev",
+    jobsearchApi: "https://jobsearch.api.jobtechdev.se",
+    jobsearchApiKey: "an APIKey",
+    production: true,
+    employerApiAccessPassword: undefined,
+    tlsSubDomainName: "jobbometern-tsl",
+    forceBrandingMode: "valid values for forcing the branding: Tsl, Default"
+};
