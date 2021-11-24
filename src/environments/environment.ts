76fe6272108a7639dcc741b798a14b1673fe8662
Simon Benjaminsson <benjaminsson@gmail.com>
2021-11-24 14:06:39 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    employersApi: "https://bitter-wave-4f8f.simonbe.workers.dev",
+    employersApi: "https://test.bitter-wave-4f8f.simonbe.workers.dev",
     jobsearchApi: "https://jobsearch.api.jobtechdev.se",
     jobsearchApiKey: "apa2",
     production: true,
